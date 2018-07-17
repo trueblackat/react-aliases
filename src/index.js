8c87d1cce996a034a66ebb5ab5b0829cdc750673
Amir Gafurov <gafurovamir@gmail.com>
2018-07-17 16:55:32 +0400

    hash router add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'normalize-css';
 import './stylesheets/index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>, document.getElementById('root'));
+  </HashRouter>, document.getElementById('root'));
