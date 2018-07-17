19bb7f09b1c9a347b3e6218b257adf18e5363b08
Amir Gafurov <gafurovamir@gmail.com>
2018-07-17 16:19:25 +0400

    path fixes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import 'normalize-css';
 import './stylesheets/index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
